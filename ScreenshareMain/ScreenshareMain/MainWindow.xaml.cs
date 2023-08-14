3998ef4e9e8d4c49731d8d7d73ea71daf52db53a
ScaraMouche <114559016+Leviachann@users.noreply.github.com>
2023-08-14 12:20:23 +0400

    Add files via upload

---
+++ ScreenshareMain/ScreenshareMain/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ServerApp
+{
+    public partial class MainWindow : Window
+    {
+        private TcpListener listener;
+        private TcpClient client;
+        private NetworkStream stream;
+        private BinaryReader reader;
+        private int screenshotCounter = 0;
+        private readonly string screenshotPath = "Screenshots/";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListenButton_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                StartListening();
+            });
+        }
+
+        private void StartListening()
+        {
+            listener = new TcpListener(IPAddress.Any, 12345);
+            listener.Start();
+
+            Dispatcher.Invoke(() =>
+            {
+                ListenButton.IsEnabled = false;
+            });
+
+            while (true)
+            {
+                client = listener.AcceptTcpClient();
+                stream = client.GetStream();
+                reader = new BinaryReader(stream);
+
+                ThreadPool.QueueUserWorkItem(o =>
+                {
+                    ReceiveAndDisplayScreenshot();
+                });
+            }
+        }
+
+        private void ReceiveAndDisplayScreenshot()
+        {
+            try
+            {
+                int imageSize = reader.ReadInt32();
+                byte[] imageBytes = reader.ReadBytes(imageSize);
+
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                using (Bitmap bmp = new Bitmap(ms))
+                {
+                    string screenshotFilename = $"{screenshotPath}Screenshot_{screenshotCounter++}.jpg";
+                    bmp.Save(screenshotFilename);
+                    Console.WriteLine($"Received and saved: {screenshotFilename}");
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        DisplayScreenshot(bmp);
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error receiving screenshot: {ex.Message}");
+            }
+        }
+
+        private void DisplayScreenshot(Bitmap screenshot)
+        {
+            ScreenshotImage.Source = ConvertBitmapToBitmapImage(screenshot);
+        }
+
+        private BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+    }
+}
---
+++ ScreenshareServer/ScreenshareServer/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ClientApp
+{
+    public partial class MainWindow : Window
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private BinaryWriter writer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.1", 12345);
+                stream = client.GetStream();
+                writer = new BinaryWriter(stream);
+
+                // Enable UI for sending screenshots
+                Dispatcher.Invoke(() =>
+                {
+                    LiveButton.IsEnabled = true;
+                    ConnectButton.IsEnabled = false;
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to the server: " + ex.Message);
+            }
+        }
+
+        private void LiveButton_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                while (true)
+                {
+                    TakeAndSendScreenshot();
+                    Thread.Sleep(200); // Adjust the interval as needed
+                }
+            });
+        }
+
+        private void TakeAndSendScreenshot()
+        {
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            using (Bitmap bmp = new Bitmap((int)screenWidth, (int)screenHeight))
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+                MemoryStream ms = new MemoryStream();
+                bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                byte[] imageBytes = ms.ToArray();
+                writer.Write(imageBytes.Length);
+                writer.Write(imageBytes);
+            }
+        }
+    }
+}
